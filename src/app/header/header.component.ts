73cb718a964a4f35f48b8d654c2afb9f85fec3d0
Chad Zhuang <changda0616@gmail.com>
2016-10-01 11:01:46 +0800

    event binding

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  color = "red";
   title = "chad's verison";
-
+  subtitle = "記載著 Will 在網路世界的學習心得與技術分享"
   constructor() { }
 
   ngOnInit() {
   }
-
+  changeTitle($event){
+    console.log($event);
+    this.title = this.title + "!";
+  }
 }
